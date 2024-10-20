ba974be62a516f36e513015a15852747f81d4855
Josh Pion <jp8117@gmail.com>
2024-10-20 18:45:05 -0400

    Updated where am I service

--- codes.jpion/Services/WhereAmIService.cs
+++ codes.jpion/Services/WhereAmIService.cs
@@ -2,11 +2,19 @@
 {
     public class WhereAmIService
     {
+        private IConfiguration _config;
+
+        public WhereAmIService(IConfiguration config)
+        {
+            _config = config;
+        }
+
         public string AppRoot
         {
             get
             {
-                var root = Environment.GetEnvironmentVariable("ROOT_URL");
+                var root = Environment.GetEnvironmentVariable("ROOT_URL") ?? 
+                    _config.GetValue<string>("WhereAmIService:rootUrl");
 
                 if (root != null)
                     return root;
