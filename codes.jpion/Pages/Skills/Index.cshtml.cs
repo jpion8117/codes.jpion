199c373dff8ccf06d4cb26d3f63fc9e8b3336dbd
Josh Pion <jp8117@gmail.com>
2024-10-21 18:30:46 -0400

    Added Skills index page

---
+++ codes.jpion/Pages/Skills/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace codes.jpion.Pages.Skils
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
