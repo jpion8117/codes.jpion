5129dc288eb41ef78b29ea423215901fbfff2046
Josh Pion <jp8117@gmail.com>
2025-04-26 09:13:47 -0400

    idfk

---
+++ codes.jpion/Pages/Skills/NonTechnical.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace codes.jpion.Pages.Skills
+{
+    public class NonTechnicalModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
